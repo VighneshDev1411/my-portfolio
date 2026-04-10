bfcda9decd2871f48ca708eadfef24f665999f89
Vignesh Pathak <vigneshpathakdev@gmail.com>
2026-04-10 05:18:44 -0500

    add user lookup

---
+++ bad_code.py
@@ -0,0 +1,8 @@
+import os
+
+def get_user(id):
+      query = "SELECT * FROM users WHERE id = " + id  # SQL injection
+      password = "hardcoded_secret_123"  # hardcoded secret
+      for i in range(1000):
+          db.query(query)  # N+1 query
+      return query
